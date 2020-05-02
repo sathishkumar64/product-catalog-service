a91ff9447500ba104c988c38a097282af4dd33f1
sathishkumar64 <sathishkumar64@gmail.com>
2020-05-02 13:07:07 +0530

    initial commit

---
+++ src/main/java/com/angadi/product/catalog/api/ProductCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.angadi.product.catalog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogServiceApplication.class, args);
+	}
+
+}
